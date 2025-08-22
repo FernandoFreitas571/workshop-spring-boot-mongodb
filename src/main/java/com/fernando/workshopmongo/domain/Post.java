735057dfb717427ca9bd93a5d014f54b09b38780
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-22 19:11:06 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/fernando/workshopmongo/domain/Post.java
+++ src/main/java/com/fernando/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.fernando.workshopmongo.domain;
 
 import com.fernando.workshopmongo.dto.AuthorDTO;
+import com.fernando.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,8 +19,10 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
     public Post(){
 
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
