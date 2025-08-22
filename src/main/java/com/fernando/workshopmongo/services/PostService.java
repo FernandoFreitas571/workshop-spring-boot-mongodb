453d7316569e91e7bd60c8c71ffba30a7494a44b
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-22 20:38:28 -0300

    Consulta com vários critérios

--- src/main/java/com/fernando/workshopmongo/services/PostService.java
+++ src/main/java/com/fernando/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.fernando.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +21,13 @@ public class PostService {
         Optional<Post> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
     public List<Post> findByTitle(String text){
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
